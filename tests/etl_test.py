80161b6436866cce13d90c1a62f1740cbffb40e8
Rasheed19 <ibraheem.abdulrasheed@gmail.com>
2024-09-10 22:38:54 +0100

    add summ tests

--- tests/etl_test.py
+++ tests/etl_test.py
@@ -1,12 +1,32 @@
 import pandas as pd
+import pytest
+
 from utils.definitions import Definition
+from utils.processor import get_summary_data
 
 
-def test_column_existence():
+@pytest.fixture
+def ncr() -> pd.DataFrame:
+    return pd.read_csv(filepath_or_buffer="./data/ncr_data_cleaned.csv")
 
-    ncr_data = pd.read_csv(filepath_or_buffer="./data/ncr_data_cleaned.csv")
-    difference = pd.Index(Definition.COLUMNS_NEEDED.values()).difference(
-        ncr_data.columns
+
+def test_column_existence(ncr):
+    difference = pd.Index(Definition.COLUMNS_NEEDED.values()).difference(ncr.columns)
+
+    assert difference.empty
+
+
+def test_summary_types(ncr):
+    summary_data = get_summary_data(ncr)
+
+    assert all(
+        [
+            isinstance(summary_data.all_day_chargers, int),
+            isinstance(summary_data.counties, list),
+            isinstance(summary_data.county_with_most_chargers, str),
+            isinstance(summary_data.in_service_chargers, int),
+            isinstance(summary_data.non_payment_chargers, int),
+            isinstance(summary_data.top_ten, dict),
+            isinstance(summary_data.total_chargers, int),
+        ]
     )
-
-    assert difference.empty is True
